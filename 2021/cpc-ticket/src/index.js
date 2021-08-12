13a539a510d3a5b8098018cb241cea4765bba66e
Drawcard <1435638+drawcard@users.noreply.github.com>
2021-08-12 14:55:56 +0930

    Challenge complete

--- 2021/cpc-ticket/src/index.js
+++ 2021/cpc-ticket/src/index.js
@@ -1,4 +1,7 @@
-import { gsap } from "gsap";
-import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import Scrambler from 'scrambling-letters/js/scrambler';
 
-gsap.registerPlugin(MotionPathPlugin);
+Scrambler({
+  target: '.data',
+  random: [100, 2000],
+  speed: 75
+});
